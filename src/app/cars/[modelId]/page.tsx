4baa7d3c510c29ae27781f02ed1be89b582d1e6f
Philipe Almeida <philipe.almeida@diconium.com>
2024-01-11 13:04:42 +0000

    feat: using fetch in this dynamic route

--- src/app/cars/[modelId]/page.tsx
+++ src/app/cars/[modelId]/page.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+import { User } from "@/app/entities/user";
+import React, { useState, useEffect } from "react";
 
 function CarDetailPage() {
-  return <div>CarDetailPage</div>;
+  const [data, setData] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        const data = await response.json();
+
+        setData(data);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error!</div>;
+
+  return (
+    <ul>
+      {data.map((item: User) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default CarDetailPage;
+  
